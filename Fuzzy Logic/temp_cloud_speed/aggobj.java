56dae47395c07149ec4746867b63d60df1571087
Maxnuggets <jericholongabela@gmail.com>
2022-05-28 10:12:42 +0800

    included aggobj.java for defuzzification and aggregation functions

---
+++ Fuzzy Logic/temp_cloud_speed/aggobj.java
@@ -0,0 +1,97 @@
+public class aggobj {
+    void aggragate(double speed_r1, double speed_r2){
+        double maxr1 = speed_r1;
+        double maxr2 = speed_r2;
+
+        double[] r1 = new double[101];
+        double[] r2 = new double[101];
+        double[] agg = new double[101];
+
+        getr1(maxr1, r1);
+        getr2(maxr2, r2);
+
+        /**Display speed- r1- r2 */
+        System.out.println("Speed - R1 - R2");
+        for(int i=0;i<101;i++){
+            System.out.printf("%d - %.4f - %.4f%n", i, r1[i], r2[i]);
+        }
+
+        /**aggragate array r1 and r2 by getting the max value for each array */
+        for(int i=0;i<101;i++){
+            if(r1[i]>r2[i]){
+                agg[i] = r1[i];
+            }
+            else{
+                agg[i]  =r2[i];
+            }
+        }
+
+        getcentroid(agg);
+
+    }
+    /*this funtion will assign value to 101 array include [0] of r1*/
+    void getr1(double maxr1, double[] r1){
+        for(int i=0;i<101;i++){
+            if(i<=25){
+                r1[i] = 0;
+            }
+            else{
+                r1[i] = 0.02 * i + -0.5; /*multiple by the linear equation of fast */
+                if(r1[i]>maxr1){    /*if value exceed 0.75, it will clip it to 0.75 */
+                    r1[i] = maxr1;
+                }
+            }
+        }
+    }
+    void getr2(double maxr2, double[] r2){
+        for(int i=0;i<101;i++){
+            if(i<=25){
+                r2[i] = maxr2;
+            }
+            if(i>25 || i<75){
+                r2[i] = -0.02 * i + 1.5; /*multiple by the linear equation of slow */
+                if(r2[i]>maxr2){ /**if vaalue exceed max r2, clip to value of max r2 */
+                    r2[i] = maxr2;
+                }
+            }
+            if(i>74){
+                r2[i] = 0;
+            }
+            
+        }
+    }
+    void getcentroid(double[] agg){
+
+        double[] product = new double[101];
+        double sum_agg = 0, sum_product = 0, centroid;
+
+        /**multiply speed to agg value 1 by 1 and assign to product */
+        for(int i=0;i<101;i++){
+            product[i] = i * agg[i];
+        }
+        System.out.printf("%n%n");
+        System.out.println("Speed - Aggregate = Product");
+        for(int i=0;i<101;i++){
+            /*System.out.println(i + " - " + agg[i] + " = " + product[i]);*/
+            System.out.printf("%d - %.4f = %.4f%n", i, agg[i], product[i]);
+        }
+
+
+        /**add all the value of product and assign to sum_product */
+        for(int i=0;i<101;i++){
+            sum_product = sum_product + product[i];
+        }
+        System.out.printf("Total Product of Speed and Aggregated R1,R2: %.4f %n", sum_product);
+
+        /**sum of aggregated r1 AND r2*/
+        for(int i=0;i<101;i++){
+            sum_agg = sum_agg + agg[i];
+        }
+        /*System.out.println("Total Aggregated R1,R2: " +sum_agg);*/
+        System.out.printf("Total Aggregated R1,R2: %.4f %n", sum_agg);
+
+        /**compute centroid */
+        centroid = sum_product/sum_agg;
+        System.out.printf("Centroid: %.4f %n", centroid);
+    }
+}
--- Fuzzy Logic/temp_cloud_speed/mainRunner.java
+++ Fuzzy Logic/temp_cloud_speed/mainRunner.java
@@ -3,12 +3,11 @@ import java.lang.Math;
 
 public class mainRunner {
     public static void main (String[] args) {
-        double cloudcover, dom_partly, dom_sunny, dom_overcast, temp, dom_freeze, dom_cool, dom_warm, dom_hot, dom_fast, dom_slow, r1_max, r2_max;
+        double cloudcover, dom_partly, dom_sunny, dom_overcast, temp, dom_freeze, dom_cool, dom_warm, dom_hot, speed_r1, speed_r2;
         tempObj domTemp = new tempObj();
         cloudObj domCloud = new cloudObj();
-        speedObj domSpeed = new speedObj();
+        aggobj aggragate = new aggobj();
         Scanner scan = new Scanner(System.in);
-        int speed = 60;
 
         System.out.print("Enter a temperature in fahrenheit: ");
         temp = scan.nextDouble();
@@ -27,11 +26,13 @@ public class mainRunner {
         dom_partly = domCloud.getPartly(cloudcover);
         dom_overcast = domCloud.getOvercast(cloudcover);
 
-        dom_fast = domSpeed.getSpeedFast(speed);
-        dom_slow = domSpeed.getSpeedSlow(speed);
+        speed_r1 = getR1Max(dom_warm, dom_sunny);
+        speed_r2 = getR2Max(dom_partly, dom_cool);
 
-        r1_max = getR1Max(dom_warm, dom_sunny);
-        r2_max = getR2Max(dom_partly, dom_cool);
+/*round to 4 decimal places*/
+        speed_r1 = Math.round(speed_r1 * 10000.0)/10000.0;
+        speed_r2 = Math.round(speed_r2 * 10000.0)/10000.0;
+
 /*        
         System.out.println("\nThe degree of membership results for a temperature of " + Math.round(temp) + "°F are:");
         System.out.println("Freezing is: " + dom_freeze + " or " + (Math.round(dom_freeze * 100)) + "%"
@@ -45,34 +46,24 @@ public class mainRunner {
         System.out.println("Partly Cloudy Weather Percentage is: " + (format(dom_partly)+"%"));
         System.out.println("Overcast Weather Percentage is: " + (format(dom_overcast)+"%") +"\n");
 */
-        System.out.println("Fast minimum is " + r1_max + " with a dom_warm of " + dom_warm + ", dom_sunny of "+ dom_sunny);
-        System.out.println("Slow minimum is " + r2_max + " with a dom_partly of " + dom_partly + ", dom_cool of "+ dom_cool);
+        System.out.println("Fast is " + speed_r1 + " with a dom_warm of " + dom_warm + ", dom_sunny of "+ dom_sunny);
+        System.out.println("Slow is " + speed_r2 + " with a dom_partly of " + dom_partly + ", dom_cool of "+ dom_cool);
 
-        System.out.println("DOM of fast is "+ dom_fast+ " with a speed of "+speed);
-        System.out.println("DOM of slow is "+ dom_slow+ " with a speed of "+speed);
+        aggragate.aggragate(speed_r1, speed_r2);
     }
     
-    /*
-    * formats double values into percentage value
-    * @param DOM in 0-1 value
-    * @returns percentage value
+    /*formats double values into percentage value
+    *@param DOM in 0-1 value
+    *@returns percentage value
     */
     static double format(double value){
         return value = Math.round(value * 100);
     }
 
-    /*
-    * @param dom_warm, dom_sunny
-    * @returns minimum of dom_warm, dom_sunny
-    */
     static double getR1Max(double dom_warm, double dom_sunny){
         return Math.min(dom_warm, dom_sunny);
     }
 
-    /*
-    * @param dom_partly, dom_cool
-    * @returns minimum of dom_partly, dom_cool
-    */
     static double getR2Max(double dom_partly, double dom_cool){
         return Math.min(dom_partly, dom_cool);
     }
